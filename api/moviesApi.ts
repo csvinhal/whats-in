102f9d664d469b2a9d36ec4c89a74eff8f89d025
csvinhal <cristiano.vinhal10@gmail.com>
2021-03-25 20:28:39 -0300

    remove gitpages and add api url

--- api/moviesApi.ts
+++ api/moviesApi.ts
@@ -3,15 +3,15 @@ import { MovieDetails } from '../models/movieDetails'
 import axios from '../utils/axios'
 
 export const getMovies = (title: string) => {
-    return axios.get<MovieResponse>(`/movies`, {
+    return axios.get<MovieResponse>(`/api/movies`, {
         params: { title },
     })
 }
 
 export const getMovieDetail = (slug: string) => {
-    return axios.get<MovieDetails>(`/movie-detail/${slug}`)
+    return axios.get<MovieDetails>(`/api/movie-detail/${slug}`)
 }
 
 export const updateMovieFavorite = (slug: string, favorite: boolean) => {
-    return axios.post<MovieDetails>(`/movie-detail/${slug}`, { favorite })
+    return axios.post<MovieDetails>(`/api/movie-detail/${slug}`, { favorite })
 }
