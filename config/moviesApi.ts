e8c8f27b45dfc73f5227f7b4dd963fba9fd39b05
csvinhal <cristiano.vinhal10@gmail.com>
2021-03-26 20:33:39 -0300

    revert

--- config/moviesApi.ts
+++ config/moviesApi.ts
@@ -1,17 +1,20 @@
 import { MovieResponse } from '../models/movie'
 import { MovieDetails } from '../models/movieDetails'
-import axios from '../utils/axios'
+import { axiosInstance, backendAxiosInstance } from '../utils/axios'
 
 export const getMovies = (title: string) => {
-    return axios.get<MovieResponse>(`/api/movies`, {
+    return axiosInstance.get<MovieResponse>(`/api/movies`, {
         params: { title },
     })
 }
 
 export const getMovieDetail = (slug: string) => {
-    return axios.get<MovieDetails>(`/api/movie-detail/${slug}`)
+    return backendAxiosInstance.get<MovieDetails>(`/api/movie-detail/${slug}`)
 }
 
 export const updateMovieFavorite = (slug: string, favorite: boolean) => {
-    return axios.post<MovieDetails>(`/api/movie-detail/${slug}`, { favorite })
+    return backendAxiosInstance.post<MovieDetails>(
+        `/api/movie-detail/${slug}`,
+        { favorite },
+    )
 }
