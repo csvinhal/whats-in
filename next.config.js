2737f0470f09fa9edb6a03ad0d6b85942c2c1363
csvinhal <cristiano.vinhal10@gmail.com>
2021-03-25 23:48:56 -0300

    add vercel url

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    env: { baseUrl: process.env.BASE_URL },
+    env: {
+        baseUrl:
+            process.env.process.env.VERCEL_URL ||
+            process.env.process.env.BASE_URL,
+    },
     images: {
         domains: ['m.media-amazon.com'],
         deviceSizes: [320, 576, 768, 992, 1200],
